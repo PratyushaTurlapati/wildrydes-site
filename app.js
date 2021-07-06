1d76a67ead54b03190632b00e003f9bc7fde3cb6
PratyushaTurlapati <65211915+PratyushaTurlapati@users.noreply.github.com>
2021-07-06 08:50:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,34 +1,50 @@
-var app = angular.module('loginapp', []);
-app.controller('signupController', function($scope,$http){
+const nodemailer = require('nodemailer')
+const { google } = require('googleapis')
+const config = require('./config.js')
+const OAuth2 = google.auth.OAuth2
 
-  $scope.signup = function(reg){
-  	$http({
-  		method : 'POST',
-  		url : 'postsignup',
-  		data : $scope.reg
-  	}).then( function success(response){
-      alert('Registered Successfully');
-      $scope.reg = {};
-  	}, function error(response){
-      alert('Registration Failed, Please try again');
-  	});
-  }
-});
+const OAuth2_client = new OAuth2(config.clientId, config.clientSecret)
+OAuth2_client.setCredentials( { refresh_token : config.refreshToken } )
 
-app.controller('loginController', function($scope,$http,$window){
+function send_mail(name, recipient) {
+    const accessToken = OAuth2_client.getAccessToken()
 
-	$scope.login = function(log){
-  	$http({
-      method:'POST',
-      url: '/postlogin',
-      data: $scope.log
-    }).then(function success(response){
-      //alert('login successfull');
-      $window.location.href = '/home'
-    }, function error(response){
-      alert('invalid credentials');
-      $scope.log = {};
-    });
-  }
+    const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            type: 'OAuth2',
+            user: config.user,
+            clientId: config.clientId,
+            clientSecret: config.clientSecret,
+            refreshToken: config.refreshToken,
+            accessToken: accessToken
+        }
+    })
 
-})
+    const mail_options = {
+        from: `THE G.O.A.T <${config.user}>`,
+        to: recipient,
+        subject: 'A Message From The G.O.A.T',
+        html: get_html_message(name)
+    }
+
+
+    transport.sendMail(mail_options, function(error, result) {
+        if (error) {
+            console.log('Error: ', error)
+        } else {
+            console.log('Success: ', result)
+        }
+        transport.close()
+    })
+}
+
+function get_html_message(name) {
+    return `
+        <h3> ${name}! You're Awesome. </h3>
+    `
+}
+
+send_mail('name here', 'email here')
+/index.css
+None
