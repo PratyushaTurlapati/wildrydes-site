4b49a3888bb98ec5b652711dffd5da47603121f0
PratyushaTurlapati <65211915+PratyushaTurlapati@users.noreply.github.com>
2021-07-10 07:33:33 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,50 +1,8 @@
-const nodemailer = require('nodemailer')
-const { google } = require('googleapis')
-const config = require('./config.js')
-const OAuth2 = google.auth.OAuth2
+const app = express();
+const port = 8080;
 
-const OAuth2_client = new OAuth2(config.clientId, config.clientSecret)
-OAuth2_client.setCredentials( { refresh_token : config.refreshToken } )
+app.get('/', (req, res) => res.send('Hello Welcome to my World!'));
+app.get('/', (req, res) => res.send('Hello Welcome to our project!'));
 
-function send_mail(name, recipient) {
-    const accessToken = OAuth2_client.getAccessToken()
-
-    const transport = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            type: 'OAuth2',
-            user: config.user,
-            clientId: config.clientId,
-            clientSecret: config.clientSecret,
-            refreshToken: config.refreshToken,
-            accessToken: accessToken
-        }
-    })
-
-    const mail_options = {
-        from: `THE G.O.A.T <${config.user}>`,
-        to: recipient,
-        subject: 'A Message From The G.O.A.T',
-        html: get_html_message(name)
-    }
-
-
-    transport.sendMail(mail_options, function(error, result) {
-        if (error) {
-            console.log('Error: ', error)
-        } else {
-            console.log('Success: ', result)
-        }
-        transport.close()
-    })
-}
-
-function get_html_message(name) {
-    return `
-        <h3> ${name}! You're Awesome. </h3>
-    `
-}
-
-send_mail('name here', 'email here')
-/index.css
-None
+app.listen(port);
+console.log(`App running on http://localhost:${port}`);
