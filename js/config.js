61d1e67caa966444730fafbaa35e0f5bc4c4d8f7
PratyushaTurlapati <65211915+PratyushaTurlapati@users.noreply.github.com>
2021-07-10 08:51:41 +0530

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-west-2_Aa8jcGaFo', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '4o8pn4mfdm9397k4i9bvj8vpo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://main.d1ormo0cg6obv5.amplifyapp.com/' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
