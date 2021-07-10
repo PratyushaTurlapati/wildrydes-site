4cc9a7bbd51f1b2243650bd5f8b4a8e983d77bf5
PratyushaTurlapati <65211915+PratyushaTurlapati@users.noreply.github.com>
2021-07-10 07:39:36 +0530

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-const app = express();
-const port = 8080;
-
-app.get('/', (req, res) => res.send('Hello Welcome to my World!'));
-app.get('/', (req, res) => res.send('Hello Welcome to our project!'));
-
-app.listen(port);
-console.log(`App running on http://localhost:${port}`);
